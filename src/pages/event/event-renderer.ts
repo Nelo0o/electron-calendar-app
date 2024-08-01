3b48e90e9e90e38a821425fa716911825c768105
Léon <leon.gallet@gmail.com>
2024-08-01 17:09:07 +0200

    Fix fuseau horaire with date.utc

--- src/pages/event/event-renderer.ts
+++ src/pages/event/event-renderer.ts
@@ -1,5 +1,3 @@
-import { ipcRenderer } from "electron";
-
 (async () => {
     const idEvt = await window.electron.getEventId()
 
@@ -9,17 +7,13 @@ import { ipcRenderer } from "electron";
     const description = document.getElementById('eventDescription');
     const date = document.getElementById('eventDate');
     
-        window.electron.getEventById(idEvt).then(event => {
-            console.log(event[0]);
+    window.electron.getEventById(idEvt).then(event => {
+        console.log(event[0]);
     
-            titre.setAttribute('value', event[0].titre);
-            description.innerHTML= event[0].description;
-            const dateFormat = event[0].date.split("/");
-            const dateOK = new Date(+dateFormat[2], parseInt(dateFormat[1]) - 1, +dateFormat[0]); 
-            date.setAttribute('value', dateOK.toISOString().substring(0, 10));
-            
-        });
+        titre.setAttribute('value', event[0].titre);
+        description.innerHTML= event[0].description;
+        const dateFormat = event[0].date.split("/");
+        const dateOK = new Date(Date.UTC(+dateFormat[2], parseInt(dateFormat[1]) - 1, +dateFormat[0])); 
+        date.setAttribute('value', dateOK.toISOString().substring(0, 10));
+    });
 })()
-
-
-

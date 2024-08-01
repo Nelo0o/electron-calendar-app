9b52eeb2cd21b6c56148f40a642c15fb15b92c50
Jérémy 81 <zoriko137@protonmail.com>
2024-08-01 14:20:36 +0200

    Fen even data ok

--- src/pages/event/event-renderer.ts
+++ src/pages/event/event-renderer.ts
@@ -1,12 +1,21 @@
 console.log("event renderer");
 
+const titre = document.getElementById('eventTitle');
+const description = document.getElementById('eventDescription');
+const date = document.getElementById('eventDate');
+
 document.addEventListener('DOMContentLoaded', () => {
-    const eventId = 10;
-    window.modale.getEventById(eventId).then(event => {
-        console.log(event);
+    const levent = window.modale.getEventById(10).then(event => {
+        console.log(event[0]);
+
+        titre.setAttribute('value', event[0].titre);
+        description.innerHTML= event[0].description;
+        let dateFormat = event[0].date;
+        dateFormat = dateFormat.split("/");
+        dateFormat = new Date(+dateFormat[2], dateFormat[1] - 1, +dateFormat[0]); 
+        date.setAttribute('value', dateFormat.toISOString().substring(0, 10));
         
     });
 });
 
-const titre = document.getElementById('eventTitle');
-titre.setAttribute('value', 'HELLLO');
+

06a7d9bf05bdbc572285e2cb7b77f051edcf28c7
Jérémy 81 <zoriko137@protonmail.com>
2024-08-02 15:34:27 +0200

    Fix

--- src/pages/importICS/ics-renderer.ts
+++ src/pages/importICS/ics-renderer.ts
@@ -3,40 +3,22 @@ console.log('ICS-RENDERER');
 (async () => {
     const lesEvents = await window.electron.getEventFromICS()
     
+        const divEvenements = document.getElementById('lesevents');
+        let contenuHTML = '';
+    
+        lesEvents.forEach((evenement) => {
+            contenuHTML += `
+                <br>
+                <h2>${evenement.titre}</h2>
+                <p>${evenement.description}</p>
+                <p>Date : ${evenement.date}</p>
+                <p>Heure : ${evenement.time}</p>
+                <br>
+                <hr>
+            `;
+        });
+    
+        divEvenements.innerHTML = contenuHTML;
+    
 })()
 
-const lesevents = [
-    {
-        titre: "titre1",
-        description: "description1",
-        date: "date1",
-        time: "time1",
-    },
-    {
-        titre: "titre2",
-        description: "description2",
-        date: "date2",
-        time: "time2",
-    },
-]
-
-function afficherEvenements() {
-    const divEvenements = document.getElementById('lesevents');
-    let contenuHTML = '';
-
-    lesevents.forEach((evenement) => {
-        contenuHTML += `
-            <br>
-            <h2>${evenement.titre}</h2>
-            <p>${evenement.description}</p>
-            <p>Date : ${evenement.date}</p>
-            <p>Heure : ${evenement.time}</p>
-            <br>
-            <hr>
-        `;
-    });
-
-    divEvenements.innerHTML = contenuHTML;
-}
-
-document.addEventListener('DOMContentLoaded', afficherEvenements);

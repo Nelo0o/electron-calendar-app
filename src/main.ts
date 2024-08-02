5a19fb9b1af6417ca184ca084bd085816bff9ccf
quentin lourenco <quentin.lourenco@livecampus.tech>
2024-08-02 11:40:00 +0200

    menu

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain, ipcRenderer } from 'electron';
+import { app, BrowserWindow, ipcMain, ipcRenderer, Menu, shell } from 'electron';
 import path from 'path';
 import { CreateDb, importDB} from "./services/database";
 import { getEventById } from './services/readDB';
@@ -30,6 +30,52 @@ const createWindow = () => {
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
+
+  const menuTemplate: Electron.MenuItemConstructorOptions[] = [
+    {
+        label: 'Event',
+        submenu: [
+            {
+                label: 'Ajouter un event',
+                click: () => {
+                  console.log('Ajouter un event');
+                },
+            },
+        ],
+    },
+    {
+        label: 'Import/Export',
+        submenu: [
+            {
+                label: 'Import ics',
+                click: () => {
+                    console.log('Import ICS');
+                },
+            },
+            {
+                label: 'Export ics',
+                click: () => {
+                    console.log('Export ICS');
+                },
+            },
+        ],
+    },
+    {
+      label: 'A propos',
+      submenu: [
+          {
+              label: 'Repo Github',
+              click: () => {
+                shell.openExternal('https://github.com/Nelo0o/electron-calendar-app');
+              },
+          },
+      ],
+  },
+];
+
+const menu = Menu.buildFromTemplate(menuTemplate);
+Menu.setApplicationMenu(menu);
+
 };
 
 ipcMain.on('open-event-modal', (event, arg) => {
